f2663a20db91a1234998286ed578834368b77b41
Jek-Konservator <roma_alieksieiev.02@mail.ru>
2021-09-17 20:05:02 +0300

    Рабата над регистрацией(Разработа окна по шаблону)

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,32 +1,46 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   StyledLoadingCard,
   StyledLoadingCardButtom,
-  StyledLoadingCardInput,
-  StyledLoadingCardTitle,
+  StyledLoadingCardInput, StyledLoadingCardTabList, StyledLoadingCardTabTab, StyledLoadingCardTitle,
   StyledLogin,
 } from "./style";
-import {Button, TextField} from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
+import { TabList, TabPanel, TabContext } from "@mui/lab";
+import Tab from "@mui/material/Tab";
 
 export const Login = () => {
-    const [login, setLogin] = useState("");
-    const [password, setPassword] = useState("");
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [value, setValue] = useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <StyledLogin>
-      <StyledLoadingCard>
-        <StyledLoadingCardTitle>
-          <>Добро пожаловать</>
-        </StyledLoadingCardTitle>
-        <StyledLoadingCardInput>
-          <TextField>asdasd</TextField>
-          <TextField>222</TextField>
-          <TextField>asdasd</TextField>
-        </StyledLoadingCardInput>
-        <StyledLoadingCardButtom>
-          <Button>asd2</Button>
-        </StyledLoadingCardButtom>
-      </StyledLoadingCard>
+      <StyledLoadingCardTitle>Авторизация</StyledLoadingCardTitle>
+      <TabContext value={value}>
+        <StyledLoadingCard>
+          <StyledLoadingCardTabList onChange={handleChange}>
+            <StyledLoadingCardTabTab label="ПО EMAIL" value="1" />
+            <StyledLoadingCardTabTab label="ПО ТЕЛЕФОНУ" value="2" />
+          </StyledLoadingCardTabList>
+
+          <TabPanel value="1">
+            <StyledLoadingCardInput>
+              <TextField label={"Логин"} />
+              <TextField label={"Пороль"} />
+            </StyledLoadingCardInput>
+          </TabPanel>
+          <TabPanel value="2">
+            <StyledLoadingCardButtom>
+              <Button>asd2</Button>
+            </StyledLoadingCardButtom>
+          </TabPanel>
+        </StyledLoadingCard>
+      </TabContext>
     </StyledLogin>
   );
 };
--- src/components/login/style.js
+++ src/components/login/style.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 import { Card } from "@material-ui/core";
+import { TabList} from "@mui/lab";
+import Tab from "@mui/material/Tab";
+
+
+
 
 export const StyledLogin = styled.div`
   width: 100%;
@@ -10,7 +15,7 @@ export const StyledLogin = styled.div`
   align-items: center;
 `;
 export const StyledLoadingCard = styled(Card)`
-  padding: 15px 0 20px 0;
+  padding: 0 0 20px 0;
   width: 450px;
   height: 600px;
   display: flex;
@@ -18,11 +23,18 @@ export const StyledLoadingCard = styled(Card)`
   justify-content: space-between;
   align-items: center;
 `;
+
 export const StyledLoadingCardTitle = styled.div`
   font-size: 25px;
 `;
 export const StyledLoadingCardInput = styled.div`
   display: flex;
   flex-direction: column;
+`;
+export const StyledLoadingCardTabList = styled(TabList)`
+ width: 100%;
+`;
+export const StyledLoadingCardTabTab = styled(Tab)`
+ width: 50%;
 `;
 export const StyledLoadingCardButtom = styled.div``;
